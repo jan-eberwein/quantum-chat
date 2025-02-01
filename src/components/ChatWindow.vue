c6f69925e6191ee62d7a1cfb3e866372cac32094
Jan Eberwein <S2410629003@fhooe.at>
2025-02-01 23:34:12 +0100

    styling adjustments

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -27,7 +27,7 @@
         >
           <p
               :class="{
-              'bg-blue-500 text-white': message.senderId === currentUser?.$id,
+              'bg-blue-400 text-white': message.senderId === currentUser?.$id,
               'bg-gray-200 text-black': message.senderId !== currentUser?.$id,
             }"
               class="inline-block p-2 rounded-lg"
@@ -54,9 +54,12 @@
       />
       <button
           @click="sendMessage"
-          class="ml-2 bg-blue-500 text-white p-2 rounded-lg"
+          class="ml-2 bg-blue-400 text-white p-2 rounded-lg flex gap-1 font-bold hover:bg-blue-500"
       >
-        Send
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+</svg>
+Send
       </button>
     </div>
   </div>
